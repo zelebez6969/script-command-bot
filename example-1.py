5a99257b7013dc7c935ab3ff338a1fc74ce98dcf
zelebez6969 <cosadiz6969@gmail.com>
2018-06-28 21:29:33 +0800

    Update

--- example-1.py
+++ example-1.py
@@ -1,3 +1,4 @@
+# daripada gada kerjaan ya buat2 aja :v
 #------------------------------------------------------------------------------------------------------------------------------------
                         elif cmd.startswith("gcastvoice "):
                                 bctxt = cmd.replace("gcastvoice ", "")
@@ -8,4 +9,14 @@
                                 n = client.getGroupIdsJoined()
                                 for apalah in n:
                                     client.sendAudio(apalah, 'tts.mp3')
+#------------------------------------------------------------------------------------------------------------------------------------
+                        elif cmd == 'square' or cmd == ' squares': #find join square
+                            a = client.getJoinedSquares()
+                            squares = a.squares
+                            txt2 = '「 Squares 」\n'
+                            for s in range(len(squares)):
+                                txt2 += "\n"+str(s+1)+". "+str(squares[s].name)
+                            txt2 += "\n\nTotal {} Squares.".format(str(len(squares)))
+                            txt2 += "\n\nUsage : Square#num"
+                            client.sendMessage(to,str(txt2))
 #------------------------------------------------------------------------------------------------------------------------------------
