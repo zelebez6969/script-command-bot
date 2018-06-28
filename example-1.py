bd1b6da7e24f901235337622119bf081b9720784
zelebez6969 <cosadiz6969@gmail.com>
2018-06-28 21:31:58 +0800

    openqr by num group

--- example-1.py
+++ example-1.py
@@ -20,3 +20,21 @@
                             txt2 += "\n\nUsage : Square#num"
                             client.sendMessage(to,str(txt2))
 #------------------------------------------------------------------------------------------------------------------------------------
+                        elif cmd.startswith("openqr"): #openqr
+                            number = cmd.replace("openqr","")
+                            groups = client.getGroupIdsJoined()
+                            try:
+                                group = groups[int(number)-1]
+                                G = client.getGroup(group)
+                                try:
+                                    G.preventedJoinByTicket = False
+                                    client.updateGroup(G)
+                                    gurl = "https://line.me/R/ti/g/{}".format(str(client.reissueGroupTicket(G.id)))
+                                except:
+                                    G.preventedJoinByTicket = False
+                                    client.updateGroup(G)
+                                    gurl = "https://line.me/R/ti/g/{}".format(str(client.reissueGroupTicket(G.id)))
+                                client.sendMessage(to, "「 Open Qr 」\n\nGroup : " + G.name + "\nLink: " + gurl)
+                            except Exception as error:
+                                client.sendMessage(to, str(error))
+#------------------------------------------------------------------------------------------------------------------------------------
